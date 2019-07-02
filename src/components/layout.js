0c04050f56cecf6ca0838f2bcb0232b8a03c38dc
Walter Macambira <wmaca@users.noreply.github.com>
2019-07-02 23:30:43 +0200

    Add logo

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,69 +1,78 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
+import Image from "gatsby-image"
 
 import { rhythm, scale } from "../utils/typography"
 
-class Layout extends React.Component {
-  render() {
-    const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
+function Layout(props) {
+  const data = useStaticQuery(graphql`
+    query LogoQuery {
+      logo: file(absolutePath: { regex: "/logo.png/" }) {
+        childImageSharp {
+          fixed(height: 110) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
 
-    const headerTitle = (
-      <Link
+  const { location, title, children } = props
+  const rootPath = `${__PATH_PREFIX__}/`
+
+  const headerTitle = (
+    <Link
+      style={{
+        boxShadow: `none`,
+      }}
+      to={`/`}
+    >
+      <Image fixed={data.logo.childImageSharp.fixed} alt={title}></Image>
+    </Link>
+  )
+
+  const header =
+    location.pathname === rootPath ? (
+      <h1
         style={{
-          boxShadow: `none`,
-          textDecoration: `none`,
-          color: `inherit`,
-        }}
-        to={`/`}
-      >
-        {title}
-      </Link>
-    )
-
-    const header =
-      location.pathname === rootPath ? (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-            textAlign: `center`,
-          }}
-        >
-          {headerTitle}
-        </h1>
-      ) : (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-            textAlign: `center`,
-          }}
-        >
-          {headerTitle}
-        </h3>
-      )
-
-    return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          ...scale(1.5),
+          marginBottom: rhythm(1.5),
+          marginTop: 0,
+          textAlign: `center`,
         }}
       >
-        <header>{header}</header>
-        <main>{children}</main>
-        <footer style={{ textAlign: `center`, color: `#999` }}>
-          © Copyright {new Date().getFullYear()},{" "}
-          <a href="https://macambira.co">Walter Macambira</a>. Powered by{" "}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>.
-        </footer>
-      </div>
+        {headerTitle}
+      </h1>
+    ) : (
+      <h3
+        style={{
+          fontFamily: `Montserrat, sans-serif`,
+          marginTop: 0,
+          textAlign: `center`,
+        }}
+      >
+        {headerTitle}
+      </h3>
     )
-  }
+
+  return (
+    <div
+      style={{
+        marginLeft: `auto`,
+        marginRight: `auto`,
+        maxWidth: rhythm(24),
+        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+      }}
+    >
+      <header>{header}</header>
+      <main>{children}</main>
+      <footer style={{ textAlign: `center`, color: `#999` }}>
+        © Copyright {new Date().getFullYear()},{" "}
+        <a href="https://macambira.co">Walter Macambira</a>. Powered by{" "}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>.
+      </footer>
+    </div>
+  )
 }
 
 export default Layout
